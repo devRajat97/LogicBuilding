1b1a650e84519782775eb475b26b0b7194e9f0ac
rajat dev <rajatdev971@gmail.com>
2024-10-13 16:47:39 +0530

    factorial

---
+++ Other/factorial.js
@@ -0,0 +1,12 @@
+// Find the factorial of given number ?
+
+let num = 6
+ ;
+let factorial = 1;
+
+for( let i = 1 ; i <= num ; i++){
+
+    factorial = factorial * i     
+
+}
+console.log(factorial)
