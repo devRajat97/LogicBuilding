58d8c47e90aabadf5f061cab3e9c94c62605e4e3
rajat dev <rajatdev971@gmail.com>
2024-12-11 23:40:29 +0530

    moveZeroesToEnd

--- Array/test.js
+++ Array/test.js
@@ -1,11 +1,4 @@
-let arr = [1,4,6,7,8,4,6,8]
-let newLength = 0
-
-for(let i = 0 ; i< arr.length; i++){
-    if( arr.indexOf(arr[i]) == i ){
-        arr[newLength] = arr[i]
-        newLength++   
-}
-}
-arr.length = newLength
-console.log(arr)
+let abc = 5
+console.log(abc)
+abc += 1
+console.log(abc)
