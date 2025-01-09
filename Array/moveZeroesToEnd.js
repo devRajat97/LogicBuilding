2505bf44573b56f7c5ec6a4a031c6c0f9eb1824b
rajat dev <rajatdev971@gmail.com>
2025-01-09 15:53:26 +0530

    moveZeroesToEnd

--- Array/moveZeroesToEnd.js
+++ Array/moveZeroesToEnd.js
@@ -1,3 +1,14 @@
-let arr = [1,3,6,8,0,4,0,4,2]
-let str = "helloworld"
-// console.log(str)
+let arr = [1, 3, 6, 8, 0, 4, 0, 4, 2];
+
+for (let i = 0; i < arr.length - 1; i++) {
+    for( let j = 0 ; j < arr.length -1; j++){
+        if (arr[j] === 0) {
+            let temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }
+    }
+ 
+}
+
+console.log(arr);
